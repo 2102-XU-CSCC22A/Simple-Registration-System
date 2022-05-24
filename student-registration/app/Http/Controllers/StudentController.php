795114be548c86ed053215c063526e74b6a77583
Raychelle Pagarigan <103406532+raychellepagarigan@users.noreply.github.com>
2022-05-24 22:18:50 +0800

    Feature/7 be getall student method (#6)
    * Feature-BE-7 : GetAll Student Method
    
    * Feature-BE-7 : GetAll Student Method Updated
    
    Co-authored-by: joshpaculba <20190016936@my.xu.edu.ph>

--- student-registration/app/Http/Controllers/StudentController.php
+++ student-registration/app/Http/Controllers/StudentController.php
@@ -3,11 +3,33 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Student;
 
 class StudentController extends Controller
 {
     public function index()
     {
         return view('template.students');
+    }
+
+    public function getAll()
+    {
+        $students = Student::getAll();
+
+        $data = [];
+        foreach($students as $s => $student)
+        {
+            $data[] = [
+                'id'            => $student->id, 
+                'id_number'     => $student->id_number,
+                'first_name'    => $student->first_name,
+                'middle_name'   => $student->middle_name,
+                'last_name'     => $student->last_name,
+                'course'        => $student->course,
+                'year_level'    => $student->year_level,
+            ];
+        }
+        
+        return response()->json(['data' => $data], 200);
     }
 }
